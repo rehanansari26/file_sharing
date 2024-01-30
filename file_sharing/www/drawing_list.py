566f2929c3b7bb23f229d03e3343faf9a7e564aa
erpadmin@preciholesports.com <erpadmin@preciholesports.com>
2024-01-30 17:21:17 +0530

    fix: Watermark rendering using server side script

---
+++ file_sharing/www/drawing_list.py
@@ -0,0 +1,25 @@
+import frappe
+
+def get_context(context):
+    if not 'Supplier' in frappe.get_roles():
+        frappe.throw("This page is only accessible to suppliers.", frappe.PermissionError)
+
+    context.user_type = frappe.db.get_value('User', frappe.session.user, 'user_type')
+    if not context.user_type == "Website User":
+        frappe.throw("You are not a valid user for this operation.", frappe.PermissionError)
+
+    context.supplier = frappe.db.get_value('Portal User', {'user': frappe.session.user, 'parenttype': 'Supplier'}, 'parent')
+    if not context.supplier:
+        frappe.throw("You are not registered as a portal user.", frappe.PermissionError)
+
+    context.drawing_permissions = frappe.db.get_all('Drawing Permission',{'attached_to_name': context.supplier, 'status': 'Shared'}, pluck='name') or None
+    if not context.drawing_permissions:
+        frappe.throw("No drawings have been shared with you.", frappe.PermissionError)
+
+    drawing_permission_details_1 = frappe.db.get_all('Drawing Permission Item',{'parent': ['in', context.drawing_permissions], 'child_status': 'Shared', 'date_based_sharing': 0},['name', 'child_item_code', 'child_item_code.item_name as item_name', 'file_url', 'views', 'views_allowed', 'parent'])
+    drawing_permission_details_2 = frappe.db.get_all('Drawing Permission Item',{'parent': ['in', context.drawing_permissions], 'child_status': 'Shared', 'date_based_sharing': 1, 'from_date': ['<=', frappe.utils.nowdate()]},['name', 'child_item_code', 'child_item_code.item_name as item_name', 'file_url', 'views', 'views_allowed', 'parent'])
+    
+    combined_drawing_permission_details = drawing_permission_details_1 + drawing_permission_details_2
+    
+    unique_drawing_permission_details = {v['name']: v for v in combined_drawing_permission_details}.values()
+    context.drawing_permission_details = list(unique_drawing_permission_details)
