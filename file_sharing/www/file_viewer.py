cacf6d03bb1f2cdd13da391439859c5ba9b806a3
rehanansari26 <rehanansari26@example.com>
2024-02-07 17:07:45 +0530

    fix: Python error

--- file_sharing/www/file_viewer.py
+++ file_sharing/www/file_viewer.py
@@ -1,8 +1,7 @@
 import frappe
 
 def get_context(context):
-    context.file_url, context.allow_download, context.parent,
-    context.child_status, context.is_private = frappe.db.get_value(
+    context.file_url, context.allow_download, context.parent, context.child_status, context.is_private = frappe.db.get_value(
         'File Permission Item',
         frappe.form_dict.get('query'),
         ['file_url', 'allow_download', 'parent', 'child_status', 'is_private']
