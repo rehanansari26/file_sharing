e5b9e5a9b2d655989e5d1e604d0b6239b81f1eeb
rehanansari26 <rehanansari26@example.com>
2024-02-10 11:32:37 +0530

    feat: file settings page

---
+++ file_sharing/file_sharing/doctype/file_settings/test_file_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Rehan Ansari and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestFileSettings(FrappeTestCase):
+	pass
